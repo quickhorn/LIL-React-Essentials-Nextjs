b6ddaceeb80a2564b4f396f439d0ac1636bad6dd
quickhorn <quickhorn@gmail.com>
2025-11-11 11:07:29 -0700

    initial project

---
+++ next-react-project/src/app/contact/page.js
@@ -0,0 +1,11 @@
+export default function Page(){
+    return(
+    <main>
+        <h1>Contact Us</h1>
+        <ul style={{ listStyleType:"none" }}>
+            <li>Phone: 888-555-5555</li>
+            <li>Email: contact@us.com</li>
+        </ul>
+    |</main>
+    )
+}
---
+++ next-react-project/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <h1>More React to Come!</h1>  
+    </main>
+  );
+}
